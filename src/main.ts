52f765ab83c4d3fbecee874663928c3ee2b602f8
verrygustiandrea <verrygustiandrea@gmail.com>
2021-06-01 02:59:39 +0800

    ssl

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+
+//ssl path
+const https = require("https");
+const fs = require("fs");
+var key = fs.readFileSync("ssl/private.key");
+var cert = fs.readFileSync("ssl/certificate.crt");
+var ca = fs.readFileSync("ssl/ca_bundle.crt");
+
+var httpsOptions = {
+  key: key,
+  cert: cert,
+  ca: ca,
+};
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.useGlobalPipes(new ValidationPipe());
   // app.useStaticAssets(path.join(__dirname, '/../public'));
   // app.use(express.static(join(process.cwd(), '../client/dist/')));
